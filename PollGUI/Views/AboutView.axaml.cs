7c3d2debc0a55826eafc6cf48005451224cfce38
IceThorn <zhengyi0119@outlook.com>
2025-02-23 15:22:56 +0800

    1

---
+++ PollGUI/Views/AboutView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PollGUI.Views;
+
+public partial class AboutView : UserControl
+{
+    public AboutView()
+    {
+        InitializeComponent();
+    }
+}
